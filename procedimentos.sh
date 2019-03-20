c6441b1c22a638fdc6f74a09010c7e6dd905b88c
andresuriane@gmail.com <suriane@eco018.econs.localdomain>
2019-03-20 17:22:37 -0300

    First commit

---
+++ procedimentos.sh
@@ -0,0 +1,6 @@
+# Faça isso já dentro do diretório desejado
+git init 
+git remote add -f origin 'https://github.com/suriane/learningR.git'
+git add .
+git commit -m "First commit"
+git push origin master
